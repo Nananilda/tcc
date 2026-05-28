e77562a203ccebcd3e2c721d24ffa8b3324c61af
Nananilda <anna.frezarini@aluno.senai.br>
2026-05-28 15:36:22 -0300

    comentários

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -24,3 +24,22 @@ try {
     die("Erro de conexão: " . $e->getMessage());
 
 }
+
+
+// index.php
+// validação de identificação (login/0 e credencial de acesso, criar js
+// adicionar if 
+
+// senha: gabi.moura, gabi3A@@
+// senha: bianca, 123
+
+// administrador
+// confere graficos (1 grafico por sensor) - grafico_sensores.php
+// ve relatórios (aplicar filtros de relatórios) - relatorio_qualidade.php
+// sensores (ve se o sensor está ativo ou não + adiciona novos sensores) - gestao_sensores.php
+// registrar (aplicar validação e tirar o "sair do site") - UsuarioController.php
+
+// funcionário
+// confere graficos (1 grafico por sensor) - grafico_sensores.php
+// ve relatórios (aplicar filtros de relatórios) - relatorio_qualidade.php
+// sensores (ve se o sensor está ativo ou não) - gestao_sensores.php
