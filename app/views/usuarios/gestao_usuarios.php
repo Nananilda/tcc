bd3a507f0faf0aa1cfe9bb7f8053e82ebc07ac8d
Anna Hilda <anna.frezarini@aluno.senai.br>
2026-07-15 16:51:11 -0300

    atualização

---
+++ app/views/usuarios/gestao_usuarios.php
@@ -0,0 +1,12 @@
+<h1> Bem-vindo a gestão de usuários</h1>
+
+        <a href="../usuarios/cadastro.php">Realizar Cadastro</a><br><br>
+  
+    
+        <a href="../usuarios/editar.php">Editar Usuário</a><br><br>
+
+    
+        <a href="../usuarios/excluir.php">Excluir Cadastro</a><br><br>
+
+
+<a href="../dashboard/painel.php">← Voltar ao dashboard</a><br>
